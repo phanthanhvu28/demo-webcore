ff6d96035dee9883ee376ce6f89658d9ac70238b
Phan Thanh Vu <dieter.vu@vela.com.vn>
2024-02-07 10:19:44 +0700

    feat: init web core

---
+++ demo-webcore/Constants/Common.cs
@@ -0,0 +1,24 @@
+﻿namespace demo_webcore.Constants
+{
+    public class Common
+    {
+        public struct Service
+        {
+            public const string Name = "Service:Name";
+        }
+        public struct Serilog
+        {
+            public const string TraceId = "TraceId";
+            public const string Environment = "Environment";
+            public const string Service = "ServiceName";
+        }
+        public struct Grafana
+        {
+            public const string LokiUrl = "Grafana:Loki:Url";
+
+            public const string TempoUrl = "Grafana:Tempo:Url";
+            public const string TempoService = "Grafana:Tempo:ServiceName";
+            public const string TempoSource = "Grafana:Tempo:SourceName";
+        }
+    }
+}
---
+++ demo-webcore/UseSerilog.cs
@@ -0,0 +1,59 @@
+﻿using Serilog;
+using Serilog.Sinks.Grafana.Loki;
+using System.Diagnostics;
+
+namespace demo_webcore
+{
+    public static class UseSerilog
+    {
+        private const string OutputTemplate =
+       "[{Timestamp:dd-MM-yyyy HH:mm:ss}] [{Level:u3}] [{Environment}] [{ServiceName}] [{TraceId}] {Message}{NewLine}{Exception}";
+
+        public static readonly string DefaultTraceId = ActivityTraceId.CreateRandom().ToString();
+
+        public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder,
+            IConfiguration configuration)
+        {
+            string serviceName = builder.GetServiceName();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty(Constants.Common.Serilog.Environment, builder.Environment.EnvironmentName)
+                .Enrich.WithProperty(Constants.Common.Serilog.TraceId, DefaultTraceId)
+                .Enrich.WithProperty(Constants.Common.Serilog.Service, serviceName)
+                .Enrich.WithMachineName()
+                .Enrich.WithEnvironmentName()
+                // TODO: disable write log to Loki. Devops guy said because this make memory leak
+                .WriteTo.GrafanaLoki(
+                    configuration[Constants.Common.Grafana.LokiUrl]!,
+                    GetLogLabel(builder.Environment.EnvironmentName, serviceName),
+                    credentials: null)
+                .WriteTo.Console(outputTemplate: OutputTemplate)
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+            return builder;
+        }
+        private static string GetServiceName(this WebApplicationBuilder builder)
+        {
+            ConfigurationManager configuration = builder.Configuration;
+            return configuration[Constants.Common.Service.Name] ?? builder.Environment.ApplicationName;
+        }
+        private static List<LokiLabel> GetLogLabel(string environmentName, string serviceName)
+        {
+            List<LokiLabel> list = new()
+        {
+            new LokiLabel
+            {
+                Key = "env",
+                Value = environmentName
+            },
+            new LokiLabel
+            {
+                Key = "service",
+                Value = serviceName
+            }
+        };
+            return list;
+        }
+    }
+}
